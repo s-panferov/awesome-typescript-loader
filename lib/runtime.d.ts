64a7ff77747c3dd452d3362c03a2965a11dc63ed
defaude <git@defaude.info>
2015-09-09 13:12:59 +0200

    Making "useSubDirectories" and "regExp" parameters optional.

--- lib/runtime.d.ts
+++ lib/runtime.d.ts
@@ -10,7 +10,7 @@ interface WebpackRequire {
     (id: string): any;
     (paths: string[], callback: (...modules: any[]) => void): void;
     ensure(ids: string[], callback: WebpackRequireEnsureCallback, chunkName?: string): void;
-    context(directory: string, useSubDirectories: boolean, regExp: RegExp): WebpackContext;
+    context(directory: string, useSubDirectories?: boolean, regExp?: RegExp): WebpackContext;
 }
 
 interface WebpackContext extends WebpackRequire {
