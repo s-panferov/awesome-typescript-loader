f37dacb9f02a76b0f05c4c0abceae75e677a6662
Guntars Asmanis-Graham <gunchars@gmail.com>
2017-10-10 17:42:55 -0400

    feat: allow setting getCustomTransformers as a path to a module
    When using forked checker mode, it's impossible to pass a custom
    function to the checker via the options as it's a different process. To
    get around this, we can pass a path to the module that is then exceuted
    once the checker is initialized.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -28,7 +28,7 @@ export interface LoaderConfig {
     debug?: boolean;
     reportFiles?: string[];
     context?: string;
-    getCustomTransformers?(): ts.CustomTransformers | undefined;
+    getCustomTransformers?: string | (() => ts.CustomTransformers | undefined);
 }
 
 export interface OutputFile {
